172c4a565542b509a76f975917de4fc7129984e6
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-02-23 22:49:10 +0530

    modify sum_digits file

--- Recursion_Sum_Digits.py
+++ Recursion_Sum_Digits.py
@@ -1,10 +1,10 @@
 def sum(num):
     if num < 0:
          return "Invalid Input"
-    elif num == 0:
-        return 0
+    elif num < 10:
+        return num
     else:
-        return(num%10) + sum(num//10)
+        return num % 10 + sum(num // 10)
     
 num = int(input("Enter a number: "))
-print(sum(num))
+print(f"The sum of the digits of {num} is {sum(num)}")
