5915fc5845b14d805262dc050ebf11622b1c09c3
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-02-25 11:21:39 +0530

    add code

--- test.py
+++ test.py
@@ -24,11 +24,18 @@
 # print(bool([]))
 # print(bool(''))
 
-for i in range(5,0,-1):
-    for j in range(i):
-        print("* ",end=" ")
-    print("")
-for i in range(6):
-    for j in range(i):
-        print("* ",end = " ")
-    print(" ")
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("* ",end=" ")
+#     print("")
+# for i in range(6):
+#     for j in range(i):
+#         print("* ",end = " ")
+#     print(" ")
+
+list = []
+num = 5
+for i in range(num):
+    list.append(i)
+
+print(list)
