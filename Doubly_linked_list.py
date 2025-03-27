ea7b2f8e501ccd6dcd85e26db3365874d01b4408
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-03-27 23:39:24 +0530

    add doubly linked list code

--- Doubly_linked_list.py
+++ Doubly_linked_list.py
@@ -1,8 +1,42 @@
 class Node:
-    def __init__(self, data, next = None, prev = None):
+    def __init__(self, prev = None, data = None, next = None):
         self.data = data
         self.next = next
         self.prev = prev
 class DoublyLinkList:
     def __init__(self):
         self.head = None
+    def print_list(self):
+        if self.head is None:
+            print("List is empty")
+            return
+        itr = self.head
+        listr = " "
+        while itr:
+            listr += str(itr.data)+"<--->" if itr.next else str(itr.data)
+            itr = itr.next
+        print(listr)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    def insert_at_begining(self, data):
+        node = Node(None, data, self.head)
+        if self.head is not None:
+            self.head.prev = node
+        self.head = node
+        
+
+
+dll = DoublyLinkList()
+dll.insert_at_begining(5)
+dll.insert_at_begining(89)
+dll.insert_at_begining(78)
+dll.insert_at_begining(56)
+dll.get_length()
+dll.print_list()
