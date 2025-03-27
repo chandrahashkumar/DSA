16aabe081bcf3556acc01d10af2cd2a807085768
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-03-27 22:32:49 +0530

    add code

---
+++ Doubly_linked_list.py
@@ -0,0 +1,8 @@
+class Node:
+    def __init__(self, data, next = None, prev = None):
+        self.data = data
+        self.next = next
+        self.prev = prev
+class DoublyLinkList:
+    def __init__(self):
+        self.head = None
