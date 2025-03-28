95dc1fe316df6293817663640cc0b87310ad4c6f
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-03-28 08:04:22 +0530

    def insert_at means index add

--- Doubly_linked_list.py
+++ Doubly_linked_list.py
@@ -31,6 +31,32 @@ class DoublyLinkList:
             self.head.prev = node
         self.head = node
         
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(None, data, None)
+            return
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(itr, data, None)
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception("Invalid index")
+        if index == 0:
+            self.insert_at_begining(data)
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(itr, data, itr.next)
+                if itr.next:
+                    itr.next.prev = node
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+
 
 
 dll = DoublyLinkList()
@@ -38,5 +64,7 @@ dll.insert_at_begining(5)
 dll.insert_at_begining(89)
 dll.insert_at_begining(78)
 dll.insert_at_begining(56)
+dll.insert_at_end(100)
+dll.insert_at(2, 45)
 dll.get_length()
 dll.print_list()
