11139c666aeecb98e18eccccb3995126618a8f55
Chandrahash Kumar <chandrahashkumar256@gmail.com>
2025-05-20 23:31:35 +0530

    Heap function

--- heap_.py
+++ heap_.py
@@ -16,8 +16,15 @@ new_element = 100
 heapq.heappushpop(heap, new_element)
 print(f"The heap after the push - pop : {heap}")
 
-s = 3
+n = 3
 input_list = [10,12,2,5,100,45,0,3]
-s_smallest = heapq.nsmallest(s, input_list)
-print(f"The smallest {s} elements are: {s_smallest}")
+s_smallest = heapq.nsmallest(n, input_list)
+print(f"The smallest {n} elements are: {s_smallest}")
+
+n_largest = heapq.nlargest(n, input_list)
+print(f"the largest {n} elements are: {n_largest}")
+
+heapq.heapify(input_list)
+print(f"The heapified list is: {input_list}")
+
 
